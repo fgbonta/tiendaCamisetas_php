03b2fb43582b42131edf54c15d6df7866bef9d5d
Fernando Bontá <fgbonta@gmail.com>
2021-07-14 22:59:13 -0300

    correccion link logo header

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -13,7 +13,7 @@
 		<header id="header">
 			<div id="logo">
 				<img src="<?=base_url?>assets/img/CamisetaRojaTrans.png" alt="camiseta logo">
-				<a href="index.php">Tienda de Camisetas</a>
+				<a href="<?=base_url?>">Tienda de Camisetas</a>
 			</div>
 		</header>
 
